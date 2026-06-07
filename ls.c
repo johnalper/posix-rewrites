bad9df2cf72120ebffe448ebadb357d615777216
John Alper <214384760+johnalper@users.noreply.github.com>
2026-06-06 23:50:47 -0400

    prints the command

---
+++ ls.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* argc is character count and argv is array strings */
+int main (int argc, char *argv[])
+{
+    /* returns the name of the command */
+    printf("%s \n", argv[0]);
+
+    return 0;
+}
+
