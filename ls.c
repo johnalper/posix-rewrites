ef659afd93fbc4389b76645cd1040b0fb2ccf2fe
John Alper <214384760+johnalper@users.noreply.github.com>
2026-06-07 21:00:26 -0400

    add path stream and error lookup

--- ls.c
+++ ls.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <dirent.h>
 
-/* argc is character count and argv is array strings */
+/* argc: character count and argv string array (pointers) */
 int main (int argc, char *argv[])
 {
-    /* returns the name of the command */
-    printf("%s \n", argv[0]);
+    /* check argument count */
+    const char *path = (argc > 1) ? argv[1] : ".";
+
+    /* open stream */
+    DIR *dir = opendir(path);
+    
+    /* non-existant directory: return error */
+    if (!dir) {
+	perror("opendir");
+
+	return 1;
+    }
 
     return 0;
 }
